8a061e4e385ef15afa4e3470c020ceea550d2b27
songbo <1152745700@qq.com>
2021-10-11 22:47:25 +0800

    新增更改后的代码

---
+++ src/main/java/org/zerhusen/dao/primary/UserAuthorityMapper.java
@@ -0,0 +1,7 @@
+package org.zerhusen.dao.primary;
+
+import org.zerhusen.core.Mapper;
+import org.zerhusen.model.primary.UserAuthority;
+
+public interface UserAuthorityMapper extends Mapper<UserAuthority> {
+}
