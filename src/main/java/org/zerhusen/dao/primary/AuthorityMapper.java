b962dea2148960bf8fa157ee09d137ee03633301
songbo <1152745700@qq.com>
2022-05-09 22:07:12 +0800

    删除历史文件

--- src/main/java/org/zerhusen/dao/primary/AuthorityMapper.java
+++
@@ -1,7 +0,0 @@
-package org.zerhusen.dao.primary;
-
-import org.zerhusen.core.Mapper;
-import org.zerhusen.model.primary.Authority;
-
-public interface AuthorityMapper extends Mapper<Authority> {
-}
